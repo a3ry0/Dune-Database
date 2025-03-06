3abb071a9685278e4053dcd38b21b63cd233831f
Audrey Do <156024592+a3ry0@users.noreply.github.com>
2025-03-05 18:45:20 -0800

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,9 +7,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_doau',
-    password        : '1484',
-    database        : 'cs340_doau'
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our applicaiton
