204840a34c2d2c66470deea6c4106dcb5d72e281
Aliyah Good <159970140+aliyahgood@users.noreply.github.com>
2025-03-10 22:03:03 -0700

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,9 +7,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_goodal',
-    password        : '5780',
-    database        : 'cs340_goodal'
+    user            : 'cs340_[]',
+    password        : '',
+    database        : 'cs340_[]'
 })
 
 // Export it for use in our applicaiton
