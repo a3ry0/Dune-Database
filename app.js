e985d146bae9c07b8b7d8545ef2b3e451b3ffb72
Audrey Do <doau@classwork.engr.oregonstate.edu>
2025-03-15 22:13:30 -0700

    Step 5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8878;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8877;                 // Set a port number at the top so it's easy to change in the future
 // Database
 var db = require('./database/db-connector')
 
