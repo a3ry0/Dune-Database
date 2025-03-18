7623c5cac1963c32fa11df5b6635f033595bbc8a
Audrey Do <doau@classwork.engr.oregonstate.edu>
2025-03-18 00:08:14 -0700

    harvester fix

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,9 +7,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_[]',
-    password        : '',
-    database        : 'cs340_[]'
+    user            : 'cs340_doau',
+    password        : '1484',
+    database        : 'cs340_doau'
 })
 
 // Export it for use in our applicaiton
